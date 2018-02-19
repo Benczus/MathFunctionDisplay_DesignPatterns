09db0e7f7ab713543b3df008a0a86b845b847a75
Bogdandy Bence <bogdandy@lab103-19.iit.uni-miskolc.hu>
2018-02-19 17:04:26 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("program ami adott intervallumba kirajzol egy függvényt adott bufferből" +
+                "\n milyen függvény" +
+                "\nintervallum" +
+                "\nlegyártja az outputot diagram formában :^)");
+
+    }
+}
